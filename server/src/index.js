e7a6faf9bec3f65fc85a504133b5b134fa604b80
YQ <yuqiu42@gmail.com>
2022-09-23 14:44:37 +0800

    Create the dummy server

--- server/src/index.js
+++ server/src/index.js
@@ -1,1 +1,13 @@
-//TODO
+const { ApolloServer } = require("apollo-server");
+
+const typeDefs = require("./schema");
+
+const server = new ApolloServer({ typeDefs });
+
+server.listen().then(() => {
+  console.log(`
+      🚀  Server is running!
+      🔉  Listening on port 4000
+      📭  Query at http://localhost:4000
+    `);
+});
