60e1016c45222b2b7dd20f04011c68201bb4bac3
YQ <yuqiu42@gmail.com>
2022-09-24 20:44:40 +0800

    Create the client query

--- client/src/pages/tracks.js
+++ client/src/pages/tracks.js
@@ -1,12 +1,48 @@
+import { gql, useQuery } from "@apollo/client";
 import React from "react";
+
 import { Layout } from "../components";
+
+/** TRACKS query to retrieve all tracks */
+const TRACKS = gql`
+  query GetTracks {
+    tracksForHome {
+      id
+      title
+      thumbnail
+      length
+      modulesCount
+      author {
+        name
+        photo
+      }
+    }
+  }
+`;
 
 /**
  * Tracks Page is the Catstronauts home page.
  * We display a grid of tracks fetched with useQuery with the TRACKS query
  */
 const Tracks = () => {
-  return <Layout grid> </Layout>;
+  const { data, loading, error } = useQuery(TRACKS);
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+  if (error) {
+    return <div>{`Error! ${error.message}`}</div>;
+  }
+  return (
+    <Layout>
+      <div>
+        {data.tracksForHome.map((track) => (
+          <div key={track.id}>
+            {track.id} {track.title}
+          </div>
+        ))}
+      </div>
+    </Layout>
+  );
 };
 
 export default Tracks;
