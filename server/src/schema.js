ebca01587089a97cc8174e841582b0f8b5f7bd61
YQ <yuqiu42@gmail.com>
2022-10-04 17:44:31 +0800

    Update schema for the track detail page

--- server/src/schema.js
+++ server/src/schema.js
@@ -4,6 +4,7 @@ const typeDefs = gql`
   type Query {
     "Get tracks array for homepage grid"
     tracksForHome: [Track!]!
+    track(id: ID!): Track
   }
 
   "A track is a group of Modules that teaches about a specific topic"
@@ -14,6 +15,12 @@ const typeDefs = gql`
     thumbnail: String
     length: Int
     modulesCount: Int
+    "The track's complete description, can be in Markdown format"
+    description: String
+    "The number of times a track has been viewed"
+    numberOfViews: Int
+    "The track's complete array of Modules"
+    modules: [Module!]!
   }
 
   "Author of a complete Track or a Module"
@@ -21,6 +28,15 @@ const typeDefs = gql`
     id: ID!
     name: String!
     photo: String
+  }
+
+  "A Module is a single unit of teaching. Multiple Modules compose a Track"
+  type Module {
+    id: ID!
+    "The Module's title"
+    title: String!
+    "The Module's length in minutes"
+    length: Int
   }
 `;
 module.exports = typeDefs;
