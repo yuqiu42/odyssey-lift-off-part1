6202290995ab30a3ad479615ccb7223a1b102184
YQ <yuqiu42@gmail.com>
2022-10-07 17:18:02 +0800

    Adding a new route to the Track page

--- client/src/index.js
+++ client/src/index.js
@@ -7,7 +7,7 @@ import GlobalStyles from "./styles";
 
 const client = new ApolloClient({
   uri: "http://localhost:4000",
-  cache: new InMemoryCache()
+  cache: new InMemoryCache(),
 });
 
 ReactDOM.render(
--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -1,12 +1,15 @@
-import React, { Fragment } from "react";
 import { Router } from "@reach/router";
+import React, { Fragment } from "react";
+
 /** importing our pages */
+import Track from "./track";
 import Tracks from "./tracks";
 
 export default function Pages() {
   return (
     <Router primary={false} component={Fragment}>
       <Tracks path="/" />
+      <Track path="/track/:trackId" />
     </Router>
   );
 }
