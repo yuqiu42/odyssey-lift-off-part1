5bcbc18e42b8d50a47d0c961dbccf1de5b132104
YQ <yuqiu42@gmail.com>
2022-10-09 23:29:19 +0800

    Add GET_TRACK query

--- client/src/pages/track.js
+++ client/src/pages/track.js
@@ -1,8 +1,44 @@
+import { gql, useQuery } from "@apollo/client";
 import React from "react";
-import { Layout } from "../components";
+
+import { Layout, QueryResult } from "../components";
+import TrackDetail from "../components/track-detail";
+
+export const GET_TRACK = gql`
+  query GetTrack($trackId: ID!) {
+    track(id: $trackId) {
+      id
+      title
+      author {
+        id
+        name
+        photo
+      }
+      thumbnail
+      length
+      modulesCount
+      numberOfViews
+      modules {
+        id
+        title
+        length
+      }
+      description
+    }
+  }
+`;
 
 const Track = ({ trackId }) => {
-  return <Layout></Layout>;
+  const { loading, error, data } = useQuery(GET_TRACK, {
+    variables: { trackId },
+  });
+  return (
+    <Layout>
+      <QueryResult error={error} loading={loading} data={data}>
+        <TrackDetail track={data?.track} />
+      </QueryResult>
+    </Layout>
+  );
 };
 
 export default Track;
