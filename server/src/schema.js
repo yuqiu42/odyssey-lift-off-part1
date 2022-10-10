5072042b84cdbb18d2cb95820736e966c0e21204
YQ <yuqiu42@gmail.com>
2022-10-10 21:17:21 +0800

    Add incrementTrackViews mutation to the schema

--- server/src/schema.js
+++ server/src/schema.js
@@ -7,6 +7,10 @@ const typeDefs = gql`
     track(id: ID!): Track
   }
 
+  type mutation {
+    incrementTrackViews(id: ID!): IncrementTrackViewsResponse!
+  }
+
   "A track is a group of Modules that teaches about a specific topic"
   type Track {
     id: ID!
@@ -37,6 +41,17 @@ const typeDefs = gql`
     title: String!
     "The Module's length in minutes"
     length: Int
+  }
+
+  type IncrementTrackViewsResponse {
+    "Similar to HTTP status code, represents the status of the mutation"
+    code: Int!
+    "Indicates whether the mutation was successful"
+    success: Boolean!
+    "Human-readable message for the UI"
+    message: String!
+    "Newly updated track after a successful mutation"
+    track: Track
   }
 `;
 module.exports = typeDefs;
